2b2841b49c74053603b0250a2133b1f9b101ce13
Shiva <RamanShiva21@gmail.com>
2024-10-10 18:46:42 -0700

    not showing up on page

---
+++ prisma/migrations/20241011005454_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "employee" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "employee_pkey" PRIMARY KEY ("id")
+);
---
+++ prisma/seed.js
@@ -0,0 +1,17 @@
+const prisma = require("../prisma");
+const seed = async () => {
+    const seed = async () => {
+        const employee = [];
+        for (let i = 0; i < 10; i++) {
+          employee.push({ name: `Employee ${i}` });
+        }
+        await prisma.employee.createMany({ data: employee });
+      };
+};
+seed()
+  .then(async () => await prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
