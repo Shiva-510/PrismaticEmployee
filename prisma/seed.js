f9718fb6aefcff02f285e72c8f8a223bfed68f9e
Shiva Raman <RamanShiva21@gmail.com>
2024-10-11 08:45:55 -0700

    Update seed.js

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,13 +1,11 @@
 const prisma = require("../prisma");
 const seed = async () => {
-    const seed = async () => {
         const employee = [];
         for (let i = 0; i < 10; i++) {
           employee.push({ name: `Employee ${i}` });
         }
         await prisma.employee.createMany({ data: employee });
       };
-};
 seed()
   .then(async () => await prisma.$disconnect())
   .catch(async (e) => {
